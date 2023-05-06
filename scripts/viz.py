ee57bd474cc4f2210e79b370bcab23f86784cd0e
Sunny Tiwari <sunny.tiwari505@gmail.com>
2023-05-07 04:38:14 +0530

    Add converters for dataframes and improve viz script

--- scripts/viz.py
+++ scripts/viz.py
@@ -3,9 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+def to_lower(x):
+    return x.lower() if isinstance(x, str) else x
 
 report_path = ""
-df = pd.read_csv(report_path, delimiter="\t\t", header=None, names=["Author", "Timestamp"])
+df = pd.read_csv(report_path, delimiter="\t\t", header=None, names=["Author", "Timestamp"], converters={"Author": to_lower})
 unique_author_list = df['Author'].unique()
 df["Timestamp"] = pd.to_datetime(df["Timestamp"], unit="s")
 df.set_index('Timestamp', inplace=True)
@@ -20,6 +22,8 @@ for author in unique_author_list:
     commits_per_month['Author'][author].plot(kind='line',  figsize=(10, 6))
     plt.title("Commits")
     plt.ylabel(author)
+
+plt.show()
 
 
 
